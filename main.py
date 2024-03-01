5c787b19f608d5418292f48086f8c7e53ed28c02
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-01 09:20:54 -0500

    Add ship class and draw ship to screen

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
 
     while True:
         screen.fill(ai_settings.bg_color)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        ship.blitme()
         pygame.display.flip()
 
 
