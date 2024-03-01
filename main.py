aa4c20e77448cab66606bee765b0e8d4dfe4088c
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-01 09:30:30 -0500

    Add game_functions module and refactor code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -12,12 +12,8 @@ def run_game():
 
     while True:
         screen.fill(ai_settings.bg_color)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         ship.blitme()
         pygame.display.flip()
-
-
 
 run_game()
