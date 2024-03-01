ebba838912f05d2aa013efd6cf91b86b4429bebd
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-01 09:11:30 -0500

    Add settings class

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     pygame.init()
-    bg_color = (230, 230, 230)
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     while True:
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
