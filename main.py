aa18aa07f321431a68a4c7c93778ef625357c8f3
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-01 08:07:27 -0500

    Add basic game loop and caption

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import sys
+import pygame
+
+def run_game():
+    pygame.init()
+    bg_color = (230, 230, 230)
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+
+
+run_game()
