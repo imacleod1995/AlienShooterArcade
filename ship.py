bd1809436ae825a3289daafb5295fde04056170b
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-01 09:22:33 -0500

    Adjusted ship position

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom - 30
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
