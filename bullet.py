f1c2e3b630e63372b0887d758a070a861b0e399a
Ian MacLeod <itmacleod1995@gmail.com>
2024-03-04 09:06:06 -0500

    Added code for bullet

---
+++ bullet.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, settings, screen, ship):
+        super().__init__()
+        self.screen = screen
+        self.rect = pygame.Rect(0, 0, settings.bullet_width, settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+        self.y = float(self.rect.y)
+        self.color = settings.bullet_color
+        self.speed_factor = settings.bullet_speed_factor
